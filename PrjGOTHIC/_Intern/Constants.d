6b6ce277220c171ad295ea299f1a2b92fadc5868
dosinabox <39790055+dosinabox@users.noreply.github.com>
2022-05-23 15:17:43 +0200

    pray hotfix

--- PrjGOTHIC/_Intern/Constants.d
+++ PrjGOTHIC/_Intern/Constants.d
@@ -765,7 +765,7 @@ const int INVCAM_Z_RING_STANDARD = 45;
 
 const int UnionActivated = 0;
 
-const string FIX_VERSION_DATE = "08/04/2022";
+const string FIX_VERSION_DATE = "23/05/2022";
 const int FIX_VERSION_START = 26;
 const int LEAST_SUPPORTED = 24;
 var int FIX_VERSION_SAVE;
